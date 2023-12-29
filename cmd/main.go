c8a3e0305ea8c6fb447dcfb390b0b116a3ae3452
obnahsgnaw <obnahsgnaw@qq.com>
2023-12-29 15:31:37 +0800

    fix(x): x

---
+++ cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/gookit/color"
+	"github.com/obnahsgnaw/application"
+	"github.com/obnahsgnaw/application/pkg/url"
+	"github.com/obnahsgnaw/application/pkg/utils"
+	"github.com/obnahsgnaw/assetweb"
+	"github.com/obnahsgnaw/assetweb/config"
+	"log"
+	"os"
+)
+
+func main() {
+	var cnf *config.Config
+	var err error
+	var app *application.Application
+	utils.RecoverHandler("asset web", func(err, stack string) {
+		if app != nil {
+			app.Logger().Error(err)
+		}
+	})
+	if cnf, err = config.Parse(); err != nil {
+		color.Error.Println("config parse failed, err=" + err.Error())
+		os.Exit(1)
+	}
+	app = application.New(application.NewCluster(cnf.Application.Id, cnf.Application.Name), cnf.Http.Name,
+		application.Debug(func() bool {
+			return false
+		}),
+		application.Logger(cnf.Log),
+	)
+	defer app.Release()
+
+	s := assetweb.New(app, cnf.Http.Name, url.New(cnf.Application.InternalIp, cnf.Http.Port),
+		assetweb.Cors(cnf.Cors),
+		assetweb.TrustedProxies(cnf.Http.TrustedProxies),
+		assetweb.RouteDebug(cnf.Http.RouteDebug),
+	)
+
+	if dir := cnf.Http.Directory(); dir != "" {
+		if err = s.RegisterDir(dir); err != nil {
+			color.Error.Println("dir failed, err=" + err.Error())
+			os.Exit(2)
+		}
+	}
+
+	app.AddServer(s)
+
+	app.Run(func(err error) {
+		color.Error.Println(err.Error())
+	})
+	app.Wait()
+
+	log.Println("Exited")
+}
---
+++ config/config.go
@@ -0,0 +1,142 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gookit/color"
+	"github.com/jessevdk/go-flags"
+	"github.com/obnahsgnaw/application/pkg/logging/logger"
+	"github.com/obnahsgnaw/application/pkg/utils"
+	"github.com/obnahsgnaw/assetweb/version"
+	"github.com/obnahsgnaw/http/cors"
+	"github.com/spf13/viper"
+	"net"
+	"os"
+	"path/filepath"
+)
+
+var Conf *Config
+
+type Config struct {
+	Version     bool   `short:"v" long:"version" description:"show version"`
+	IniFile     string `short:"c" long:"conf" description:"Ini file"`
+	Application *Application
+	Log         *logger.Config
+	Cors        *cors.Config
+	Http        *Http
+}
+
+type Application struct {
+	Id         string `long:"cluster-id" description:"Cluster id" required:"true" default:"dev"`
+	Name       string `long:"cluster-name" description:"Cluster name" required:"true" default:"Dev"`
+	InternalIp string `long:"internal-ip" description:"Server ip address"`
+	Debug      bool   `long:"debug" description:"Enable debug"`
+}
+
+type Http struct {
+	curDir         string
+	Name           string   `long:"name" description:"http name"`
+	TrustedProxies []string `long:"trusted-ip" description:"Trusted proxy ip, the gateway ip, multi"`
+	RouteDebug     bool     `long:"route-debug" description:"enable http engine to debug mode"`
+	Port           int      `long:"port" description:"http port"`
+	Dir            string   `long:"dir" description:"http dir"`
+	Current        bool     `short:"t" long:"cur" description:"http use current pwd dir"`
+}
+
+func (s *Http) Directory() string {
+	if s.Dir != "" {
+		return s.Dir
+	}
+	if s.Current {
+		return s.curDir
+	}
+	return ""
+}
+
+func configError(msg string, err error) error {
+	return utils.TitledError("config error", msg, err)
+}
+
+func Parse() (*Config, error) {
+	var opt Config
+
+	if _, err := flags.Parse(&opt); err != nil {
+		var flagErr *flags.Error
+		ok := errors.As(err, &flagErr)
+		if !ok || flagErr.Type != flags.ErrHelp {
+			color.Error.Println("config parse failed, err=" + err.Error())
+		}
+		os.Exit(0)
+	}
+	Conf = &opt
+
+	if Conf.Version {
+		fmt.Println(version.Info().String())
+		os.Exit(0)
+	}
+
+	if Conf.IniFile != "" {
+		cc := viper.New()
+		cc.SetConfigFile(Conf.IniFile)
+		if err := cc.ReadInConfig(); err != nil {
+			var configFileNotFoundError viper.ConfigFileNotFoundError
+			if errors.As(err, &configFileNotFoundError) {
+				return nil, configError("config file not found", err)
+			} else {
+				return nil, configError("parse config file failed", err)
+			}
+		}
+		if err := cc.Unmarshal(Conf); err != nil {
+			return nil, configError("decode config failed", err)
+		}
+	}
+
+	if Conf.Application == nil {
+		Conf.Application = &Application{
+			Id:         "web",
+			Name:       "web",
+			InternalIp: "",
+			Debug:      false,
+		}
+	}
+	if Conf.Application.InternalIp == "" {
+		Conf.Application.InternalIp = getLocalIp()
+	}
+
+	if Conf.Http.Name == "" {
+		Conf.Http.Name = "web"
+	}
+
+	if Conf.Http.Port <= 0 {
+		return nil, configError("port required", nil)
+	}
+
+	if Conf.Http.Dir != "" {
+		dr, err := filepath.Abs(Conf.Http.Dir)
+		if err != nil {
+			return nil, configError("dir err,", nil)
+		}
+		Conf.Http.Dir = dr
+	}
+	if Conf.Http.Current {
+		dr, err := os.Getwd()
+		if err != nil {
+			return nil, configError("current dir fetch failed", nil)
+		}
+		Conf.Http.curDir = dr
+	}
+
+	return Conf, nil
+}
+
+func getLocalIp() (ip string) {
+	var err error
+	var conn net.Conn
+
+	if conn, err = net.Dial("udp", "8.8.8.8:80"); err != nil {
+		return "127.0.0.1"
+	}
+	defer func() { _ = conn.Close() }()
+
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
